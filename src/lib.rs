9cfd1b5250eb20878bf72cc94c1db87593818e8f
Theo Mathieu <tmathieu.github@fastmail.com>
2022-09-02 12:14:19 +0200

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use boilerpipe::parse_document;
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn parse_html(a: String) -> PyResult<String> {
+    Ok(parse_document(&a).content().to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn html_tools(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(parse_html, m)?)?;
+    Ok(())
+}
